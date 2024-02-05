677ff5d0cfeafd1b7b91faab84754b7387946f1d
Reyhan Yogastama <reyhan2411@gmail.com>
2024-02-05 14:48:39 +0700

    fix generator

--- internal/util/generator.go
+++ internal/util/generator.go
@@ -1,22 +1,29 @@
 package util
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func GenerateRandomString(n int) string {
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
 	var charsets = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	letters := make([]rune, n)
 	for i := range letters {
-		letters[i] = charsets[rand.Intn(len(charsets))]
+		letters[i] = charsets[rng.Intn(len(charsets))]
 	}
 
 	return string(letters)
 }
 
 func GenerateRandomNumber(n int) string {
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
 	var charsets = []rune("0123456789")
 	letters := make([]rune, n)
 	for i := range letters {
-		letters[i] = charsets[rand.Intn(len(charsets))]
+		letters[i] = charsets[rng.Intn(len(charsets))]
 	}
 
 	return string(letters)
