29cdd39b99914ad3d49a59ae6ea7ee0b50a3bef8
Reyhan Yogastama <reyhan2411@gmail.com>
2024-02-07 17:38:55 +0700

    fix notification migration

--- db/migrations/000004_create_notifications_table.up.sql
+++ db/migrations/000004_create_notifications_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "notifications" (
     "id" bigserial PRIMARY KEY,
     "user_id" int NOT NULL,
-    "status" int,
-    "title" text,
-    "body" text,
-    "is_read" int,
-    "created_at" timestamp(0)
+    "status" int NOT NULL,
+    "title" text NOT NULL,
+    "body" text NOT NULL,
+    "is_read" int NOT NULL,
+    "created_at" timestamp(0) NOT NULL
 )
