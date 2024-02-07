da2aa56ba5b58b11cb7751775a96154228a59bc7
Reyhan Yogastama <reyhan2411@gmail.com>
2024-02-07 17:51:22 +0700

    change datatype of table notifications

--- db/migrations/000004_create_notifications_table.up.sql
+++ db/migrations/000004_create_notifications_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "notifications" (
     "id" bigserial PRIMARY KEY,
-    "user_id" int NOT NULL,
-    "status" int NOT NULL,
+    "user_id" bigint NOT NULL,
+    "status" bigint NOT NULL,
     "title" text NOT NULL,
     "body" text NOT NULL,
-    "is_read" int NOT NULL,
+    "is_read" bigint NOT NULL,
     "created_at" timestamp(0) NOT NULL
 )
