712bca5ee2f5f0945a1163ee4ffb8db71acded36
Reyhan Yogastama <reyhan2411@gmail.com>
2024-02-10 18:39:59 +0700

    add migration topup table

---
+++ db/migrations/000006_create_topup_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "topup" (
+    "id" varchar(100) PRIMARY KEY,
+    "user_id" int NOT NULL,
+    "amount" bigint NOT NULL,
+    "status" int NOT NULL,
+    "snap_url" varchar(255)
+);
+
+ALTER TABLE "topup" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
